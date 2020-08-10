c60507477812e78629037051768d4668db58bdc9
team-cake <eka.cheuk@gmail.com>
2020-08-10 16:55:06 +0200

    Made useSelector for a list of pizzas on PizzaList.js, also sorted based on popularity.

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,20 +1,37 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React from 'react'
+import { useSelector } from 'react-redux'
 
-const selectUser = reduxState => {
-  return reduxState.user;
-};
+const selectUser = (reduxState) => {
+	return reduxState.user
+}
+
+const listOfPizza = (reduxState) => {
+	return reduxState.pizzas
+}
 
 export default function PizzaList() {
-  const user = useSelector(selectUser);
-
-  return (
-    <div>
-      <h1>Pizza Explorer</h1>
-      <p>
-        Welcome back, <strong>{user.name}</strong>! Your favorite pizzas:
-      </p>
-      <p>TODO: the list of pizzas</p>
-    </div>
-  );
+	const user = useSelector(selectUser)
+	const pizzas = useSelector(listOfPizza)
+	console.log('what kind of pizza', pizzas)
+	return (
+		<div>
+			<h1>Pizza Explorer</h1>
+			<p>
+				Welcome back, <strong>{user.name}</strong>! Your favorite pizzas:
+			</p>
+			<p>TODO: the list of pizzas</p>
+			<br />
+			<p>
+                {pizzas.sort((a, b) => b.bought - a.bought)
+                .map((pizza) => {
+					return (
+						<div>
+							<ul><b>Name: {pizza.name}</b> - Ate {pizza.bought} of these!</ul> 
+                            Description: {pizza.description} 
+						</div>
+					)
+				})}
+			</p>
+		</div>
+	)
 }
